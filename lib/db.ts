d3861552d572b2331344c75d80ab7982dce16f8b
karimLa <kira00luffy@gmail.com>
2021-03-06 14:57:44 +0100

    moved checking for DB_URI inside dbConnect

--- lib/db.ts
+++ lib/db.ts
@@ -3,15 +3,15 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 const DB_URI = process.env.DB_URI
 
-if (!DB_URI) {
-	throw new Error(
-		'Please define the DB_URI environment variable inside .env.local'
-	)
-}
-
 export default async function dbConnect() {
 	if (mongoose.connection.readyState >= 1) {
 		return
+	}
+
+	if (!DB_URI) {
+		throw new Error(
+			'Please define the DB_URI environment variable inside .env.local'
+		)
 	}
 
 	await mongoose.connect(DB_URI, {
